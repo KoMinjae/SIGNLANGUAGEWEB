6713b8ea1c453c012fb75291585b21c070737527
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2020-06-19 07:46:01 +0900

    Add files via upload

--- test.js
+++ test.js
@@ -21,8 +21,10 @@ var options = {
 var sessionStore = new MySQLStore(options);
 var app = express();
 
-app.use(express.static('public'));//css파일 불러오기
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static('public')); //css파일 불러오기
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 app.use(bodyParser.json())
 app.use(session({
   secret: "!@!#!$",
@@ -35,47 +37,58 @@ var mydir = fs.readFileSync('./html/mydir.ejs', 'utf8');
 var login = fs.readFileSync('./html/login.ejs', 'utf8');
 var main = fs.readFileSync('./html/main.ejs', 'utf8');
 var signup = fs.readFileSync('./html/signup.ejs', 'utf8');
-var loginusername="";
-var checksession ="";          //로그인 세션 변수
-var lastsearch = new Array();  //최근검색어 저장 리스트
+var loginusername = "";
+var checksession = ""; //로그인 세션 변수
+var lastsearch = new Array(); //최근검색어 저장 리스트
 //첫화면 설정
 app.get('/', (req, res) => {
+  if(req.session.id==checksession){
+    console.log("test1",req.session.id,checksession);
   var page = ejs.render(main, {
-    title: "Test",
+    title: loginusername+"님의 메인화면",
+    text : loginusername+"님 환영합니다",
+    data2 : lastsearch,
   });
+}else{
+  console.log("test2",req.session.id, checksession);
+  var page = ejs.render(main, {
+    title: "메인",
+    data2 : lastsearch,
+  });
+}
   res.send(page);
-})
-app.get('/searchpage', function (req, res) {
+});
+app.get('/searchpage', function(req, res) {
   var page = ejs.render(test, {
     title: "search page",
   });
   res.send(page);
 })
-app.get('/login',function (req,res) {
+app.get('/login', function(req, res) {
   var page = ejs.render(login, {
     title: "Login",
   });
   res.send(page);
 })
-app.get('/signup',function(req,res){
-  var page=ejs.render(signup, {
+app.get('/signup', function(req, res) {
+  var page = ejs.render(signup, {
     title: "SIGNUP",
   });
   res.send(page);
 })
-app.get('/mydir',function(req,res){connection.query('SELECT img from signlanguage as A LEFT JOIN dir as B on A.slid = B.slid where userid = ?', [loginusername], function (err, rows, fields) {
-    if (!err){
+app.get('/mydir', function(req, res) {
+  connection.query('SELECT img from signlanguage as A LEFT JOIN dir as B on A.slid = B.slid where userid = ?', [loginusername], function(err, rows, fields) {
+    if (!err) {
       var page = ejs.render(test, {
         title: "good",
         data: rows,
       });
       res.send(page);
-    }
-    else
+    } else
       console.log('Error while performing Query.');
   });
 });
-connection.connect(function (err) {
+connection.connect(function(err) {
   if (!err) {
     console.log("Database is connected ... \n\n");
   } else {
@@ -83,11 +96,11 @@ connection.connect(function (err) {
   }
 });
 //단어검색에 사용
-app.post('/search1', function (req, res) {
+app.post('/search1', function(req, res) {
   console.log("test2");
 
   var body = req.body;
-  connection.query('SELECT * from signlanguage where title LIKE ?', '%' + [body.test1] + '%', function (err, rows, fields) {
+  connection.query('SELECT * from signlanguage where title LIKE ?', '%' + [body.test1] + '%', function(err, rows, fields) {
     if (!err) {
       var page = ejs.render(test, {
         title: "good",
@@ -95,17 +108,16 @@ app.post('/search1', function (req, res) {
       });
       res.send(page);
       console.log('The solution is: ', rows);
-    }
-    else
+    } else
       console.log('Error while performing Query.');
   });
 });
 //단어장에 추가 기능
-app.post('/adddir1', function (req, res) {
+app.post('/adddir1', function(req, res) {
   var body = req.body;
   console.log('addtest', req.session.id, body.test2);
-  connection.query('INSERT INTO dir(userid,slid) values(?,?)', [loginusername, body.test2], function (err, rows, fields) {
-    connection.query('SELECT img from signlanguage as A LEFT JOIN dir as B on A.slid = B.slid where userid = ?', [loginusername], function (err, row, fields) {
+  connection.query('INSERT INTO dir(userid,slid) values(?,?)', [loginusername, body.test2], function(err, rows, fields) {
+    connection.query('SELECT img from signlanguage as A LEFT JOIN dir as B on A.slid = B.slid where userid = ?', [loginusername], function(err, row, fields) {
       if (!err) {
         var page = ejs.render(mydir, {
           title: "good",
@@ -115,48 +127,116 @@ app.post('/adddir1', function (req, res) {
         console.log('The solution issss: ', rows);
 
         console.log('The solution is: ', row);
-      }
-      else
+      } else
         console.log('Error while performing Query.');
     });
   });
 });
 //회원가입 테스트
-app.post('/signup1', function (req, res) {
+app.post('/signup1', function(req, res) {
   console.log("signtest");
   var body = req.body;
-  console.log("sign in test2", body.username, body.userID, body.password);
-  connection.query('INSERT INTO user(userid,pw,name) values(?,?,?)', [body.userID, body.password, body.username], function (err, rows, fields) {
-    if (!err) {
-      console.log("sign in test1", rows);
-      console.log("sign in test2", body.userID, body.password, body.username);
-    }
-    else
-      console.log('Error while performing Query.');
-  });
-});
+  if (blankcheck(body.username, body.userID, body.password, body.password_check)) {
+    console.log(body.userID, body.password, body.username);
 
+    connection.query('INSERT INTO user(userid,pw,name) values(?,?,?)', [body.userID, body.password, body.username], function(err, rows, fields) {
+      if (!err) {
+        console.log("sign in test1", rows);
+        console.log("sign in test2", body.userID, body.password, body.username);
+        res.redirect('/');
+      } else
+        console.log('Error while performing Query.');
+    });
+  }
+});
+//아이디 중복확인
+/*app.post('/idcheck1', function(req, res) {
+  console.log("idcheck");
+  var body = req.body;
+
+  if (body.userID > 1) { //아이디 값 있는기 검사
+    connection.query('select * from user where userid = ?', [body.userID], function(err, row, fields) {
+      if (!err) {
+        if (rows)
+          console.log("1111");
+      } else {
+        console.log("2222");
+      }
+    });
+  } else {
+    console.log("아이디 공백");
+  }
+});*/
 //로그인 test
-app.post('/login1', function (req, res) {
+app.post('/login1', function(req, res) {
   console.log("logintest");
   var body = req.body;
-  loginusername = body.username;
-  if (loginusername != "") {
-    connection.query('select * from user where userid = ? and pw =?', [body.username, body.password], function (err, row, fields) {
+  if (blank(body.userID, body.password)) {
+    console.log('id', body.userID, 'pw', body.password);
+    loginusername = body.userID;
+    connection.query('select * from user where userid = ? and pw =?', [body.userID, body.password], function(err, results, rows) {
       if (!err) {
-        req.session.id = req.body.username;
-        checksession = req.session.id;
-        console.log("sign in test1", req.session.id);
-        req.session.save(function () {
-          res.redirect('/');
-        });
+        if (results != '') {
+          loginusername = results[0].name;
+          console.log(results);
+          console.log("이름", results[0].name);
+          req.session.id = req.body.userID;
+          checksession = req.session.id;
+          console.log("sign in test1", req.session.id);
+          req.session.save(function() {
+            res.redirect('/');
+          });
+        } else {
+          console.log("아이디 비번 확인");
+        }
+      } else {
+        console.log("query error");
       }
-      else if (err) console.log("login error");
     });
-  } else
-    res.send(page);
+  }
+  /*var page = ejs.render(login, {
+    title: "메인",
+    text1: "아이디와 비밀번호를 입력해주세요",
+    data2 : lastsearch,
+  });
+  res.send(page);*/
+
 });
-//문장검색에 사용
+app.get('/logout', function (req, res) {
+  console.log("로그아웃")
+  loginusername = "";
+  req.session.destroy(
+    function (err) {
+      res.redirect('/');
+    });
+
+});
+
+function blankcheck(name, id, pw, pw_ck) { //공백이랑 비밀번호 같은지 검사
+  //아이디 비밀번호 조건주기
+  if (name.length < 1 || id.length < 1 || pw.length < 1 || pw_ck.length < 1) {
+    //이름은 2글자 이상 아이디 비번 6글자 이상
+    console.log("글자수, 공백");
+    return false;
+  } else if (pw != pw_ck) {
+    //비밀번호 검사
+    console.log("비밀번호 다름");
+    return false;
+  } else {
+    console.log("통과");
+    return true;
+  }
+}
+
+function blank(id, pw) {
+  if (id.length < 1 || pw.length < 1) {
+    console.log("공백");
+    return false;
+  } else {
+    console.log("통과");
+    return true;
+  }
+}
 /*
 app.post('/search2', function(req,res){
   var body = req.body;
